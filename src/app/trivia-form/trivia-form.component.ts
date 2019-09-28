5f993a5c1d7d045661232417ff45bc235b26f3c1
Faraan Qureshi <faqfaraan@gmail.com>
2019-09-28 11:14:03 -0400

    first commit

---
+++ src/app/trivia-form/trivia-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trivia-form',
+  templateUrl: './trivia-form.component.html',
+  styleUrls: ['./trivia-form.component.css']
+})
+export class TriviaFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
